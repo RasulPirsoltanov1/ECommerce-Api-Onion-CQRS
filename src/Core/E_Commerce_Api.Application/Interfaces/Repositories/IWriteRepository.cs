d3135aae172b88d148c24f529ef3bed7f08273a9
RasulPirsotanov1 <7m0uvuf@code.edu.az>
2024-01-12 01:13:47 +0400

    Added write repository

--- src/Core/E_Commerce_Api.Application/Interfaces/Repositories/IWriteRepository.cs
+++ src/Core/E_Commerce_Api.Application/Interfaces/Repositories/IWriteRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace E_Commerce_Api.Application.Interfaces.Repositories
+﻿using E_Commerce_Api.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce_Api.Application.Interfaces.Repositories
 {
-    public interface IWriteRepository
+    public interface IWriteRepository<T> where T : EntityBase
     {
+        DbSet<T> Table { get; }
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IList<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task HardDeleteAsync(T entity);
+        Task SoftDeleteAsync(T entity);
     }
 }
