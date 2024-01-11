86692903af41fb12b96ecd61955398d540699c72
RasulPirsotanov1 <7m0uvuf@code.edu.az>
2024-01-12 02:04:54 +0400

    Added UnitOfWork design pattern successfully

---
+++ src/Core/E_Commerce_Api.Application/Interfaces/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce_Api.Application.Interfaces.Repositories;
+using E_Commerce_Api.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Api.Application.Interfaces.UnitOfWorks
+{
+    public interface IUnitOfWork : IAsyncDisposable
+    {
+        IReadRepository<T> GetReadRepository<T>() where T : EntityBase, new();
+        IWriteRepository<T> GetWriteRepository<T>() where T : EntityBase, new();
+        Task<int> SaveAsync();
+        int Save();
+    }
+}
---
+++ src/Infrastructure/E_Commerce_Api.Persistance/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using E_Commerce_Api.Application.Interfaces.Repositories;
+using E_Commerce_Api.Application.Interfaces.UnitOfWorks;
+using E_Commerce_Api.Domain.Common;
+using E_Commerce_Api.Persistance.Context;
+using E_Commerce_Api.Persistance.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce_Api.Persistance.UnitOfWorks
+{
+    internal class UnitOfWork : IUnitOfWork
+    {
+        AppDbContext dbContext;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async ValueTask DisposeAsync() => await dbContext.DisposeAsync();
+
+        public IReadRepository<T> GetReadRepository<T>() where T : EntityBase, new()
+        {
+            return new ReadRepository<T>(dbContext);
+        }
+
+        public IWriteRepository<T> GetWriteRepository<T>() where T : EntityBase, new() => new WriteRepository<T>(dbContext);
+
+        public int Save()
+        {
+            return dbContext.SaveChanges();
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await dbContext.SaveChangesAsync();
+        }
+    }
+}
